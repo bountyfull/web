4a731b4235b29d6b6a057adc403953a11280d7da
dmaddox <dmaddox@gmail.com>
2018-02-17 21:33:46 -0700

    initial page structure and nav component

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () =>
+  
+  <div className="container-fluid px-0">
+  -----
+  Nav component
+  -----
+  </div>;
+
+export default Nav;
